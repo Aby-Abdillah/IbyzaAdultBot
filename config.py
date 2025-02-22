50e2dc054c22c6e018a9b2389d44b0022ab88474
Aby Abdillah <abdillahyachya@gmail.com>
2025-02-23 01:22:19 +0700

    Fixxed For Me

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ USER_SESSION_STRING = ""
 CMD_SUFFIX = ""
 AUTHORIZED_CHATS = ""
 SUDO_USERS = ""
-DATABASE_URL = "mongodb+srv://cjdwcjdw709:<*yDQic.u.!$btw8>@cluster0.yhixv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URL = "mongodb://ibyza:<db_password>@undefined/?replicaSet=atlas-j0bieb-shard-0&ssl=true&authSource=admin"
 STATUS_LIMIT = 4
 DEFAULT_UPLOAD = "rc"
 STATUS_UPDATE_INTERVAL = 15
